6c72a71e300593a43de65c4530d0c7242c969778
justs <justs@WINDOW12>
2023-05-31 19:21:55 +1200

    Add project files.

---
+++ LoginApp/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginApp
+{
+    public partial class MainWindow : Window
+    {
+        private const string ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private const string TableName = "Users";
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please enter a username and password.");
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+
+                    string selectQuery = $"SELECT * FROM {TableName} WHERE Username = @Username AND Password = @Password";
+
+                    using (SqlCommand command = new SqlCommand(selectQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@Username", username);
+                        command.Parameters.AddWithValue("@Password", password);
+
+                        using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                        {
+                            DataTable dataTable = new DataTable();
+                            adapter.Fill(dataTable);
+
+                            if (dataTable.Rows.Count > 0)
+                            {
+                                string loggedInUsername = dataTable.Rows[0]["Username"].ToString();
+
+                                MessageBox.Show("Login successful!");
+
+                                // Navigate to the Welcome page and pass the username as a parameter
+                                Welcome welcomePage = new Welcome(loggedInUsername);
+                                welcomePage.Show();
+                                this.Close();
+
+                            }
+                            else
+                            {
+                                MessageBox.Show("Invalid username or password.");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void CreateAccountButton_Click(object sender, RoutedEventArgs e)
+        {
+            Register registerPage = new Register();
+            registerPage.Show();
+            this.Close();
+        }
+    }
+}
---
+++ LoginApp/Register.xaml.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LoginApp
+{
+    public partial class Register : Window
+    {
+        private const string ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private const string TableName = "Users";
+
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+            string confirmPassword = ConfirmPasswordBox.Password;
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(confirmPassword))
+            {
+                MessageBox.Show("Please enter a username and password.");
+                return;
+            }
+
+            if (password != confirmPassword)
+            {
+                MessageBox.Show("Passwords do not match.");
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+
+                    string insertQuery = $"INSERT INTO {TableName} (Username, Password) VALUES (@Username, @Password)";
+
+                    using (SqlCommand command = new SqlCommand(insertQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@Username", username);
+                        command.Parameters.AddWithValue("@Password", password);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Registration successful!");
+                            ClearFields();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Registration failed.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void ClearFields()
+        {
+            UsernameTextBox.Text = string.Empty;
+            PasswordBox.Password = string.Empty;
+            ConfirmPasswordBox.Password = string.Empty;
+        }
+
+        private void BackToLoginLink_Click(object sender, RoutedEventArgs e)
+        {
+            // Navigate back to the login page
+            MainWindow loginPage = new MainWindow();
+            loginPage.Show();
+            Close();
+        }
+    }
+}
+
